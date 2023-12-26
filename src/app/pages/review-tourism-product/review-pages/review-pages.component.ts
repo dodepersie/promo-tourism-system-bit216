f1a4cf0a97f211fa05876cf5469386b865e22f17
Dicky Trismanto <trismantodicky@gmail.com>
2023-12-26 10:08:55 +0800

    feat: use case 5 review tourism products

--- src/app/pages/review-tourism-product/review-pages/review-pages.component.ts
+++ src/app/pages/review-tourism-product/review-pages/review-pages.component.ts
@@ -14,11 +14,83 @@ import { Product } from 'src/app/product';
   styleUrls: ['./review-pages.component.css'],
 })
 export class ReviewPagesComponent implements OnInit {
+  productData: Product | null;
   constructor(
-
+    private paypalService: PaypalService,
+    private productService: ProductService,
+    private reviewService: ReviewService,
+    private swalService: SwalService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const purchaseId = params['id'];
+      this.paypalService.getInvoiceDBById(purchaseId).subscribe({
+        next: (purchaseData) => {
+          this.productService.getProduct(purchaseData.product_id).subscribe({
+            next: (productData) => {
+              this.productData = productData;
+              this.patchInputForm(purchaseId);
+            },
+            error: (err) => {
+              console.error(err);
+            },
+          });
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+    });
+  }
 
+  patchInputForm(purchaseId: string) {
+    const userId = localStorage.getItem('user_id');
+    if (this.productData && this.reviewForm) {
+      this.reviewForm.patchValue({
+        user_id: userId,
+        product_id: this.productData._id,
+        purchase_id: purchaseId,
+      });
+    }
+  }
+
+  reviewForm: FormGroup = this.fb.group({
+    user_id: ['', [Validators.required]],
+    product_id: ['', [Validators.required]],
+    purchase_id: ['', [Validators.required]],
+    rating: ['', [Validators.required]],
+    review: ['', [Validators.required]],
+  });
+
+  submitReview() {
+    const review: postReview = {
+      user_id: this.reviewForm.value.user_id,
+      product_id: this.reviewForm.value.product_id,
+      purchase_id: this.reviewForm.value.purchase_id,
+      rating: this.reviewForm.value.rating,
+      review: this.reviewForm.value.review,
+    };
+
+    if (this.reviewForm.invalid) {
+      this.swalService.errorSwal('Please fill all required form!..');
+      return;
+    }
+
+    // Send service
+    this.reviewService.createReview(review).subscribe({
+      next: (review) => {
+        console.log(review);
+        this.swalService.successSwal('Review submitted.. :)');
+        this.router.navigate(['/user-dashboard/purchase-history']);
+      },
+      error: (err) => {
+        console.error(err);
+        this.swalService.errorSwal(err.message);
+      },
+    });
   }
 }
